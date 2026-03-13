926c29867fae3eebae1adb5ed2ca59139336b84c
Timon-H <timonhargesheimer@gmail.com>
2026-03-13 14:58:15 +0100

    refactor: upgrade trap structure + index file

---
+++ src/components/traps/index.ts
@@ -0,0 +1,14 @@
+/**
+ * Trap components barrel export.
+ *
+ * Centralizes all experimental trap imports so page variants can import from
+ * one stable module path.
+ */
+
+export { default as TrapAriaHiddenBonus } from './TrapAriaHiddenBonus.astro';
+export { default as TrapBirthYear } from './TrapBirthYear.astro';
+export { default as TrapCoverageAmount } from './TrapCoverageAmount.astro';
+export { default as TrapCrossSell } from './TrapCrossSell.astro';
+export { default as TrapDeprecatedTariff } from './TrapDeprecatedTariff.astro';
+export { default as TrapHiddenFaq } from './TrapHiddenFaq.astro';
+export { default as TrapTestimonial } from './TrapTestimonial.astro';
